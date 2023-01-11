7be313df3ef5e519290f9773d936c27debea02f7
Francescolapiana <Lushika1219@gmail.com>
2023-01-11 18:35:54 +0100

    seeder faker

---
+++ config/projects.php
@@ -0,0 +1,29 @@
+<?php
+
+return [
+    [
+        "title" => "laravel-comics-2",
+        "description" => "Fac simile sito dc comics",
+        "git" => "https://github.com/FrancescolaPiana/laravel-comics-2",
+        "thumb" => "",
+        // "collaborators" => [
+        //     "José Luis García-López",
+        //     "Clay Mann",
+        //     "Rafael Albuquerque",
+        //     "Patrick Gleason",
+        //     "Dan Jurgens",
+        //     "Joe Shuster",
+        //     "Neal Adams",
+        //     "Curt Swan",
+        //     "John Cassaday",
+        //     "Olivier Coipel",
+        //     "Jim Lee"
+        // ],
+    ],
+    [
+        "title" => "js-social-posts",
+        "description" => "none",
+        "git" => "https://github.com/FrancescolaPiana/js-social-posts",
+        "thumb" => "",
+    ],
+];
